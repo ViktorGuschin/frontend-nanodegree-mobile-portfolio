9252fb15fc61c93fd9d3176582c6437a4ee2af32
ViktorGuschin <groftblr@gmail.com>
2019-11-24 20:31:08 +0300

    fix: fps on pizza page

--- views/js/main.js
+++ views/js/main.js
@@ -506,7 +506,11 @@ function updatePositions() {
     // document.body.scrollTop is no longer supported in Chrome.
     var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
     var phase = Math.sin((scrollTop / 1250) + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    (function (items, i) {
+      window.requestAnimationFrame(function() {
+        items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+      });
+    })(items, i)    
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
